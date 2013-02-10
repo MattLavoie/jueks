6d1134c3ffa5da1236cae1845d2e0cd02c43f63e
Matt.Lavoie <matt.lavoie@powerdms.com>
2013-02-10 17:38:10 -0500

    angular established and routes sort of there.

--- app/script/app.js
+++ app/script/app.js
@@ -1,5 +1,11 @@
-(function (undefined) {
+(function () {
     var dependencies = [];
 
-    angular.module('jukes', dependencies);
+    angular.module('jueks', dependencies)
+        .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            //when('/', {templateUrl:'views/main.html'}).
+            when('/posts/:postKey', {templateUrl:'content/UI-and-Interaction-design.html'}).
+            otherwise({redirectTo:'/posts/UI-and-Interaction-design'});
+    }]);
 })();
--- app/script/controllers/app-controller.js
+++ app/script/controllers/app-controller.js
@@ -1,9 +1,19 @@
-$(function () {
+(function () {
+    var titles = ['/ ju:/ /\'Eks/', 'You Ex', 'UX', 'jooo ehks', 'It\'s an Ex of a thing!', 'What the X?', 'jueks.com', 'Ju Eks',
+                  'jueks', 'Don\'t do that', 'You could just view source'];
+    var currentTitle = 0;
+
+
     angular.module('jueks').controller('main',
         ['$scope',
             function ($scope) {
-                $scope.foo = "hello world";
 
+                $scope.changeTitle = function () {
+                    var selection = Math.floor(Math.random() * (titles.length - 1));
+                    selection === currentTitle && selection++;
+                    currentTitle = selection;
+                    $scope.titleText = titles[selection];
+                }
 
             }]);
 })();
